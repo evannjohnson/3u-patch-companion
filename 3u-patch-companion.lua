ceb4b469d1d67b889cd3f7cb9b1d8dae201a6a37
Evan Johnson <ev.jonnson@gmail.com>
2025-04-24 16:48:54 -0600

    setting crows inputs to w/syn params, fully replicating the crow script

--- 3u-patch-companion.lua
+++ 3u-patch-companion.lua
@@ -2,7 +2,7 @@ w = crow.ii.wsyn -- less typing in repl, like crow.ii.wsyn.lpg_time(0) hey
 t = crow.ii.txo
 
 -- crow.ii.wsyn.ar_mode(1)
-crow.ii.wsyn.curve(-5)
+crow.ii.wsyn.curve(5)
 crow.ii.wsyn.ramp(0)
 crow.ii.wsyn.fm_index(0)
 -- crow.ii.wsyn.fm_env(-.35) -- this offsets the minimum CV that comes out of planar
@@ -32,79 +32,81 @@ clock.run(initTxo)
 -- vin is the value at the input, vout is the output value that voltage will be equal to
 -- values between the defined points are linearly interpolated
 -- this allows for ex. making the first half of the knob much more sensitive than the last half, for ex. fine-tuning a short envelope
--- ins = {
---     {
---         callback = function(v)
---             -- ii.wsyn.fm_env(varisponse(v, {
---             --     { vin = 0,   vout = 0 },
---             --     { vin = 0.3, vout = 0 },
---             --     { vin = 3.5, vout = 0.1 },
---             --     { vin = 6,   vout = 1 },
---             --     { vin = 10,  vout = 3 },
---             -- }))
---             crow.ii.wsyn.fm_index(varisponse(v, {
---                 { vin = 0,   vout = 0 },
---                 { vin = 0.3, vout = 0 }, -- wiggle room for left side of joystick
---                 { vin = 2.5, vout = 0.3 },
---                 { vin = 6,   vout = 1 },
---                 { vin = 10,  vout = 3 },
---             }))
---         end,
---     },
---     {
---         callback = function(v)
---             crow.ii.wsyn.lpg_time(varisponse(v, {
---                 { vin = 0,   vout = 4 },
---                 { vin = 0.1, vout = 3 },
---                 { vin = 0.5, vout = 1 },
---                 { vin = 3,   vout = -1 },
---                 { vin = 8,   vout = -3 },
---                 { vin = 10,  vout = -4 }
---             }))
---         end,
---     }
--- }
+ins = {
+    {
+        callback = function(v)
+            -- ii.wsyn.fm_env(varisponse(v, {
+            --     { vin = 0,   vout = 0 },
+            --     { vin = 0.3, vout = 0 },
+            --     { vin = 3.5, vout = 0.1 },
+            --     { vin = 6,   vout = 1 },
+            --     { vin = 10,  vout = 3 },
+            -- }))
+            crow.ii.wsyn.fm_index(varisponse(v, {
+                { vin = 0,   vout = 0 },
+                { vin = 0.3, vout = 0 }, -- wiggle room for left side of joystick
+                { vin = 2.5, vout = 0.3 },
+                { vin = 6,   vout = 1 },
+                { vin = 10,  vout = 3 },
+            }))
+        end,
+    },
+    {
+        callback = function(v)
+            crow.ii.wsyn.lpg_time(varisponse(v, {
+                { vin = 0,   vout = 4 },
+                { vin = 0.1, vout = 3 },
+                { vin = 0.5, vout = 1 },
+                { vin = 3,   vout = -1 },
+                { vin = 8,   vout = -3 },
+                { vin = 10,  vout = -4 }
+            }))
+        end,
+    }
+}
 
--- --[[
--- pos: should be 0-1
--- points: an array of tables. the tables should have 2 indexes, 'pos' and 'val'.
---     - pos: the actual knob position to map to val
---     - val: the position that would be output if the knob were set to the actual position specified by pos
---     knobPos will be smoothly mapped to the the output between the points
---     the array of tables must be sorted by the table's pos and val, and sorting by either of the tables' indices should result in the same sort
---     the last table in the array should be {pos=1,val=1}
---     ex. one point with pos .5 and value .2 would cause the first 50% of the knob to map to 0-.2, and the second half to map to .2-1
--- --]]
--- function varisponse(vin, points)
---     vin = math.max(points[1].vin, math.min(vin, points[#points].vin))
+--[[
+pos: should be 0-1
+points: an array of tables. the tables should have 2 indexes, 'pos' and 'val'.
+    - pos: the actual knob position to map to val
+    - val: the position that would be output if the knob were set to the actual position specified by pos
+    knobPos will be smoothly mapped to the the output between the points
+    the array of tables must be sorted by the table's pos and val, and sorting by either of the tables' indices should result in the same sort
+    the last table in the array should be {pos=1,val=1}
+    ex. one point with pos .5 and value .2 would cause the first 50% of the knob to map to 0-.2, and the second half to map to .2-1
+--]]
+function varisponse(vin, points)
+    vin = math.max(points[1].vin, math.min(vin, points[#points].vin))
 
---     for i = 2, #points do
---         if points[i].vin >= vin then -- hit
---             local vinBottom = points[i - 1].vin
---             local voutBottom = points[i - 1].vout
---             local vinRange = points[i].vin - vinBottom
---             local voutRange = points[i].vout - voutBottom
---             local segmentPercentage = (vin - vinBottom) / vinRange
---             local segmentVal = segmentPercentage * voutRange
+    for i = 2, #points do
+        if points[i].vin >= vin then -- hit
+            local vinBottom = points[i - 1].vin
+            local voutBottom = points[i - 1].vout
+            local vinRange = points[i].vin - vinBottom
+            local voutRange = points[i].vout - voutBottom
+            local segmentPercentage = (vin - vinBottom) / vinRange
+            local segmentVal = segmentPercentage * voutRange
 
---             return voutBottom + segmentVal
---         end
---     end
--- end
+            return voutBottom + segmentVal
+        end
+    end
+end
 
--- function startStream()
---     for i = 1, 2 do
---         crow.input[i] { mode = 'stream'
---         , time = 0.01
---         , stream = function(v) ins[i].callback(v) end }
---     end
--- end
+function startStream()
+    for i = 1, 2 do
+        -- crow.input[i] { mode = 'stream'
+        -- , time = 0.01
+        -- , stream = function(v) ins[i].callback(v) end }
+        crow.input[i].stream = function(v) ins[i].callback(v) end
+        crow.input[i].mode("stream", 0.01)
+    end
+end
 
--- function stopStream()
---     for i = 1, 2 do
---         input[i] { mode = 'none' }
---     end
--- end
+function stopStream()
+    for i = 1, 2 do
+        input[i] { mode = 'none' }
+    end
+end
 
--- startStream()
+startStream()
 
