ae48cf1b00b11436d5b5d2667930e4da9649722f
Evan Johnson <ev.jonnson@gmail.com>
2025-04-29 11:49:04 -0600

    params stepping in correct increments

--- 3u-patch-companion.lua
+++ 3u-patch-companion.lua
@@ -1,79 +1,138 @@
--- crow.ii.wsyn.ar_mode(1)
-crow.ii.wsyn.curve(5)
-crow.ii.wsyn.ramp(0)
-crow.ii.wsyn.fm_index(0)
--- crow.ii.wsyn.fm_env(-.35) -- this offsets the minimum CV that comes out of planar
--- crow.ii.wsyn.fm_env(-.5) -- this offsets the minimum CV that comes out of planar when attenuated by a mystic 0tennuator at noon
-crow.ii.wsyn.fm_env(0)
-crow.ii.wsyn.fm_ratio(4)
-crow.ii.wsyn.lpg_symmetry(-3.5)
-crow.ii.wsyn.lpg_time(-2.73)
--- crow.ii.wsyn.patch(1, 3) -- this to fm env, use planar unipolar out
--- crow.ii.wsyn.patch(2, 5) -- that to lpg time
--- mappings = {
---     crow.ii.wsyn.fm_env,
---     crow.ii.wsyn.lpg_time
+-- -- crow.ii.wsyn.ar_mode(1)
+-- crow.ii.wsyn.curve(5)
+-- crow.ii.wsyn.ramp(0)
+-- crow.ii.wsyn.fm_index(0)
+-- -- crow.ii.wsyn.fm_env(-.35) -- this offsets the minimum CV that comes out of planar
+-- -- crow.ii.wsyn.fm_env(-.5) -- this offsets the minimum CV that comes out of planar when attenuated by a mystic 0tennuator at noon
+-- crow.ii.wsyn.fm_env(0)
+-- crow.ii.wsyn.fm_ratio(4)
+-- crow.ii.wsyn.lpg_symmetry(-3.5)
+-- crow.ii.wsyn.lpg_time(-2.73)
+-- -- crow.ii.wsyn.patch(1, 3) -- this to fm env, use planar unipolar out
+-- -- crow.ii.wsyn.patch(2, 5) -- that to lpg time
+-- -- mappings = {
+-- --     crow.ii.wsyn.fm_env,
+-- --     crow.ii.wsyn.lpg_time
+-- -- }
+
+-- function initTxo()
+--     clock.sleep(1)
+--     crow.ii.txo.cv(3, 1)
+--     crow.ii.txo.osc_wave(3, 0)
+--     crow.ii.txo.env_att(3, 50)
+--     crow.ii.txo.env_dec(3, 400)
+-- end
+
+-- clock.run(initTxo)
+
+bipolar5v=controlspec.def{
+    min = -5.0,
+    max = 5.0,
+    warp = 'lin',
+    step = 0.01,
+    default = 0,
+    units = 'v',
+    quantum = 0.01,
+    wrap = false
+}
+
+
+-- params:add{
+--   id="wsyn curve",
+--   type="number",
+--   min=-5,
+--   max=5,
+--   default=5,
+--   action=function(x) crow.ii.wsyn.curve(x) end
 -- }
-
-function initTxo()
-    clock.sleep(1)
-    crow.ii.txo.cv(3, 1)
-    crow.ii.txo.osc_wave(3, 0)
-    crow.ii.txo.env_att(3, 50)
-    crow.ii.txo.env_dec(3, 400)
-end
-
-clock.run(initTxo)
-
 params:add{
   id="wsyn curve",
-  type="number",
-  min=-5,
-  max=5,
-  default=5,
+  type="control",
+  controlspec=controlspec.def{
+        min = -5.0,
+        max = 5.0,
+        warp = 'lin',
+        step = 0.01,
+        default = 5,
+        units = 'v',
+        quantum = 0.005,
+        wrap = false
+    },
+  formatter=function(param) return string.format("%.2f", param:get()) end,
   action=function(x) crow.ii.wsyn.curve(x) end
 }
 params:add{
   id="wsyn ramp",
-  type="number",
-  min=-5,
-  max=5,
-  default=0,
+  type="control",
+  controlspec=controlspec.def{
+        min = -5.0,
+        max = 5.0,
+        warp = 'lin',
+        step = 0.01,
+        default = 0,
+        units = 'v',
+        quantum = 0.005,
+        wrap = false
+    },
+  formatter=function(param) return string.format("%.2f", param:get()) end,
   action=function(x) crow.ii.wsyn.ramp(x) end
 }
 params:add{
   id="wsyn fm index",
-  type="number",
-  min=-5,
-  max=5,
-  default=0,
+  type="control",
+  controlspec=controlspec.def{
+        min = 0,
+        max = 5.0,
+        warp = 'lin',
+        step = 0.01,
+        default = 0,
+        units = 'v',
+        quantum = 0.01,
+        wrap = false
+    },
+  formatter=function(param) return string.format("%.2f", param:get()) end,
   action=function(x) crow.ii.wsyn.fm_index(x) end
 }
 params:add{
   id="wsyn fm ratio",
   type="number",
   min=1,
-  max=10,
+  max=24,
   default=4,
   action=function(x) crow.ii.wsyn.fm_ratio(x) end
 }
 params:add{
   id="wsyn lpg symmetry",
-  type="number",
-  min=-5,
-  max=5,
-  default=-3.5,
+  type="control",
+  controlspec=controlspec.def{
+        min = -5,
+        max = 5,
+        warp = 'lin',
+        step = 0.01,
+        default = -3.5,
+        units = 'v',
+        quantum = 0.005,
+        wrap = false
+    },
+  formatter=function(param) return string.format("%.2f", param:get()) end,
   action=function(x) crow.ii.wsyn.lpg_symmetry(x) end
 }
 params:add{
   id="wsyn lpg time",
-  type="number",
-  min=-5,
-  max=5,
-  default=-2.73,
-  action=function(x) crow.ii.wsyn.lpg_symmetry(x) end
+  type="control",
+  controlspec=controlspec.def{
+        min = -5,
+        max = 5,
+        warp = 'lin',
+        step = 0.01,
+        default = -2.7,
+        units = 'v',
+        quantum = 0.005,
+        wrap = false
+    },
+  formatter=function(param) return string.format("%.2f", param:get()) end,
+  action=function(x) crow.ii.wsyn.lpg_time(x) end
 }
-
 
 params:add{
   id="txo waveshape - voice 3",
@@ -82,6 +141,22 @@ params:add{
   max=4500,
   default=0,
   action=function(x) crow.ii.txo.osc_wave(3, x) end
+}
+params:add{
+  id="txo level - voice 3",
+  type="control",
+  controlspec=controlspec.def{
+        min = 0,
+        max = 8,
+        warp = 'lin',
+        step = 0.01,
+        default = 1,
+        units = 'v',
+        quantum = 0.01/8,
+        wrap = false
+    },
+  formatter=function(param) return string.format("%.2f", param:get()) end,
+  action=function(x) crow.ii.txo.cv(3, x) end
 }
 params:add{
   id="txo attack - voice 3",
@@ -96,7 +171,7 @@ params:add{
   type="number",
   min=0,
   max=10000,
-  default=1000,
+  default=2000,
   action=function(x) crow.ii.txo.env_dec(3, x) end
 }
 params:add{
@@ -123,86 +198,87 @@ params:add{
   default=1000,
   action=function(x) crow.ii.txo.env_dec(4, x) end
 }
+params:bang()
 
 -- tune these response curves to taste
 -- vin is the value at the input, vout is the output value that voltage will be equal to
 -- values between the defined points are linearly interpolated
 -- this allows for ex. making the first half of the knob much more sensitive than the last half, for ex. fine-tuning a short envelope
--- ins = {
---     {
---         callback = function(v)
---             -- ii.wsyn.fm_env(varisponse(v, {
---             --     { vin = 0,   vout = 0 },
---             --     { vin = 0.3, vout = 0 },
---             --     { vin = 3.5, vout = 0.1 },
---             --     { vin = 6,   vout = 1 },
---             --     { vin = 10,  vout = 3 },
---             -- }))
---             crow.ii.wsyn.fm_index(varisponse(v, {
---                 { vin = 0,   vout = 0 },
---                 { vin = 0.3, vout = 0 }, -- wiggle room for left side of joystick
---                 { vin = 2.5, vout = 0.3 },
---                 { vin = 6,   vout = 1 },
---                 { vin = 10,  vout = 3 },
---             }))
---         end,
---     },
---     {
---         callback = function(v)
---             crow.ii.wsyn.lpg_time(varisponse(v, {
---                 { vin = 0,   vout = 4 },
---                 { vin = 0.1, vout = 3 },
---                 { vin = 0.5, vout = 1 },
---                 { vin = 3,   vout = -1 },
---                 { vin = 8,   vout = -3 },
---                 { vin = 10,  vout = -4 }
---             }))
---         end,
---     }
--- }
+ins = {
+    {
+        callback = function(v)
+            -- ii.wsyn.fm_env(varisponse(v, {
+            --     { vin = 0,   vout = 0 },
+            --     { vin = 0.3, vout = 0 },
+            --     { vin = 3.5, vout = 0.1 },
+            --     { vin = 6,   vout = 1 },
+            --     { vin = 10,  vout = 3 },
+            -- }))
+            crow.ii.wsyn.fm_index(varisponse(v, {
+                { vin = 0,   vout = 0 },
+                { vin = 0.3, vout = 0 }, -- wiggle room for left side of joystick
+                { vin = 2.5, vout = 0.3 },
+                { vin = 6,   vout = 1 },
+                { vin = 10,  vout = 3 },
+            }))
+        end,
+    },
+    {
+        callback = function(v)
+            crow.ii.wsyn.lpg_time(varisponse(v, {
+                { vin = 0,   vout = 4 },
+                { vin = 0.1, vout = 3 },
+                { vin = 0.5, vout = 1 },
+                { vin = 3,   vout = -1 },
+                { vin = 8,   vout = -3 },
+                { vin = 10,  vout = -4 }
+            }))
+        end,
+    }
+}
 
--- --[[
--- pos: should be 0-1
--- points: an array of tables. the tables should have 2 indexes, 'pos' and 'val'.
---     - pos: the actual knob position to map to val
---     - val: the position that would be output if the knob were set to the actual position specified by pos
---     knobPos will be smoothly mapped to the the output between the points
---     the array of tables must be sorted by the table's pos and val, and sorting by either of the tables' indices should result in the same sort
---     the last table in the array should be {pos=1,val=1}
---     ex. one point with pos .5 and value .2 would cause the first 50% of the knob to map to 0-.2, and the second half to map to .2-1
--- --]]
--- function varisponse(vin, points)
---     vin = math.max(points[1].vin, math.min(vin, points[#points].vin))
+--[[
+pos: should be 0-1
+points: an array of tables. the tables should have 2 indexes, 'pos' and 'val'.
+    - pos: the actual knob position to map to val
+    - val: the position that would be output if the knob were set to the actual position specified by pos
+    knobPos will be smoothly mapped to the the output between the points
+    the array of tables must be sorted by the table's pos and val, and sorting by either of the tables' indices should result in the same sort
+    the last table in the array should be {pos=1,val=1}
+    ex. one point with pos .5 and value .2 would cause the first 50% of the knob to map to 0-.2, and the second half to map to .2-1
+--]]
+function varisponse(vin, points)
+    vin = math.max(points[1].vin, math.min(vin, points[#points].vin))
 
---     for i = 2, #points do
---         if points[i].vin >= vin then -- hit
---             local vinBottom = points[i - 1].vin
---             local voutBottom = points[i - 1].vout
---             local vinRange = points[i].vin - vinBottom
---             local voutRange = points[i].vout - voutBottom
---             local segmentPercentage = (vin - vinBottom) / vinRange
---             local segmentVal = segmentPercentage * voutRange
+    for i = 2, #points do
+        if points[i].vin >= vin then -- hit
+            local vinBottom = points[i - 1].vin
+            local voutBottom = points[i - 1].vout
+            local vinRange = points[i].vin - vinBottom
+            local voutRange = points[i].vout - voutBottom
+            local segmentPercentage = (vin - vinBottom) / vinRange
+            local segmentVal = segmentPercentage * voutRange
 
---             return voutBottom + segmentVal
---         end
---     end
--- end
+            return voutBottom + segmentVal
+        end
+    end
+end
 
--- function startStream()
---     for i = 1, 2 do
---         -- crow.input[i] { mode = 'stream'
---         -- , time = 0.01
---         -- , stream = function(v) ins[i].callback(v) end }
---         crow.input[i].stream = function(v) ins[i].callback(v) end
---         crow.input[i].mode("stream", 0.01)
---     end
--- end
+function startStream()
+    for i = 1, 2 do
+        -- crow.input[i] { mode = 'stream'
+        -- , time = 0.01
+        -- , stream = function(v) ins[i].callback(v) end }
+        crow.input[i].stream = function(v) ins[i].callback(v) end
+        crow.input[i].mode("stream", 0.01)
+    end
+end
 
--- function stopStream()
---     for i = 1, 2 do
---         input[i] { mode = 'none' }
---     end
--- end
+function stopStream()
+    for i = 1, 2 do
+        input[i] { mode = 'none' }
+    end
+end
 
--- startStream()
+startStream()
 
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Controls the patch in my 3u rack through crow, primarily controlling params of txo as an enveloped oscillator and w/syn, which are played via ansible.
+
+# todo
+- [ ] read joystick input via norns instead of crow
+- [ ] more ways of controlling fm ratio
+- [ ] test midi mapping
+- [ ] try mapping mouse to params like joystick
