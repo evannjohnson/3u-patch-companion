463c124d131f37475c36cd88c8f2d8a42985f714
Evan Johnson <ev.jonnson@gmail.com>
2025-05-01 03:13:34 -0600

    trackball controlling wsyn functional

--- 3u-patch-companion.lua
+++ 3u-patch-companion.lua
@@ -25,6 +25,8 @@
 
 -- clock.run(initTxo)
 
+local event_codes = require "hid_events"
+
 bipolar5v=controlspec.def{
     min = -5.0,
     max = 5.0,
@@ -82,16 +84,33 @@ params:add{
   type="control",
   controlspec=controlspec.def{
         min = 0,
-        max = 5.0,
+        max = 4.0,
         warp = 'lin',
         step = 0.01,
         default = 0,
         units = 'v',
-        quantum = 0.01,
+        -- quantum = 0.01,
+        quantum = 0.001,
         wrap = false
     },
   formatter=function(param) return string.format("%.2f", param:get()) end,
   action=function(x) crow.ii.wsyn.fm_index(x) end
+}
+params:add{
+  id="wsyn fm env",
+  type="control",
+  controlspec=controlspec.def{
+        min = -5,
+        max = 5,
+        warp = 'lin',
+        step = 0.01,
+        default = 0,
+        units = 'v',
+        quantum = 0.005,
+        wrap = false
+    },
+  formatter=function(param) return string.format("%.2f", param:get()) end,
+  action=function(x) crow.ii.wsyn.fm_env(x) end
 }
 params:add{
   id="wsyn fm ratio",
@@ -121,13 +140,13 @@ params:add{
   id="wsyn lpg time",
   type="control",
   controlspec=controlspec.def{
-        min = -5,
-        max = 5,
+        min = -4,
+        max = 4,
         warp = 'lin',
         step = 0.01,
         default = -2.7,
         units = 'v',
-        quantum = 0.005,
+        quantum = 0.001,
         wrap = false
     },
   formatter=function(param) return string.format("%.2f", param:get()) end,
@@ -200,42 +219,27 @@ params:add{
 }
 params:bang()
 
--- tune these response curves to taste
--- vin is the value at the input, vout is the output value that voltage will be equal to
--- values between the defined points are linearly interpolated
--- this allows for ex. making the first half of the knob much more sensitive than the last half, for ex. fine-tuning a short envelope
-ins = {
-    {
-        callback = function(v)
-            -- ii.wsyn.fm_env(varisponse(v, {
-            --     { vin = 0,   vout = 0 },
-            --     { vin = 0.3, vout = 0 },
-            --     { vin = 3.5, vout = 0.1 },
-            --     { vin = 6,   vout = 1 },
-            --     { vin = 10,  vout = 3 },
-            -- }))
-            crow.ii.wsyn.fm_index(varisponse(v, {
-                { vin = 0,   vout = 0 },
-                { vin = 0.3, vout = 0 }, -- wiggle room for left side of joystick
-                { vin = 2.5, vout = 0.3 },
-                { vin = 6,   vout = 1 },
-                { vin = 10,  vout = 3 },
-            }))
-        end,
-    },
-    {
-        callback = function(v)
-            crow.ii.wsyn.lpg_time(varisponse(v, {
-                { vin = 0,   vout = 4 },
-                { vin = 0.1, vout = 3 },
-                { vin = 0.5, vout = 1 },
-                { vin = 3,   vout = -1 },
-                { vin = 8,   vout = -3 },
-                { vin = 10,  vout = -4 }
-            }))
-        end,
-    }
-}
+function trackball_input(typ, code, val)
+  local p
+  -- hid_events.codes.REL_X = 0x00
+  if code == 0x00 then
+    p = "wsyn fm index"
+  -- hid_events.codes.REL_Y = 0x01
+  elseif code == 0x01 then
+    p = "wsyn lpg time"
+  -- hid_events.codes.REL_WHEEL = 0x08
+  elseif code == 0x08 then
+    p = "wsyn fm ratio"
+    val = -val
+  end
+
+  if p then
+    params:delta(p, val)
+    -- print("delta: "..val.." "..p..": "..params:get(p))
+  end
+
+end
+hid.vports[1].event = trackball_input
 
 --[[
 pos: should be 0-1
@@ -264,21 +268,98 @@ function varisponse(vin, points)
     end
 end
 
-function startStream()
-    for i = 1, 2 do
-        -- crow.input[i] { mode = 'stream'
-        -- , time = 0.01
-        -- , stream = function(v) ins[i].callback(v) end }
-        crow.input[i].stream = function(v) ins[i].callback(v) end
-        crow.input[i].mode("stream", 0.01)
-    end
+local response_tables = {
+  trackball_x_to_wsyn_fm_index = {
+    { vin = 0,   vout = 0 },
+    { vin = 0.3, vout = 0 }, -- wiggle room for left side of joystick
+    { vin = 2.5, vout = 0.3 },
+    { vin = 6,   vout = 1 },
+    { vin = 10,  vout = 3 },
+  }
+}
+
+
+function crow_setup_for_joystick()
+  -- tune these response curves to taste
+  -- vin is the value at the input, vout is the output value that voltage will be equal to
+  -- values between the defined points are linearly interpolated
+  -- this allows for ex. making the first half of the knob much more sensitive than the last half, for ex. fine-tuning a short envelope
+  ins = {
+      {
+          callback = function(v)
+              -- ii.wsyn.fm_env(varisponse(v, {
+              --     { vin = 0,   vout = 0 },
+              --     { vin = 0.3, vout = 0 },
+              --     { vin = 3.5, vout = 0.1 },
+              --     { vin = 6,   vout = 1 },
+              --     { vin = 10,  vout = 3 },
+              -- }))
+              crow.ii.wsyn.fm_index(varisponse(v, {
+                  { vin = 0,   vout = 0 },
+                  { vin = 0.3, vout = 0 }, -- wiggle room for left side of joystick
+                  { vin = 2.5, vout = 0.3 },
+                  { vin = 6,   vout = 1 },
+                  { vin = 10,  vout = 3 },
+              }))
+          end,
+      },
+      {
+          callback = function(v)
+              crow.ii.wsyn.lpg_time(varisponse(v, {
+                  { vin = 0,   vout = 4 },
+                  { vin = 0.1, vout = 3 },
+                  { vin = 0.5, vout = 1 },
+                  { vin = 3,   vout = -1 },
+                  { vin = 8,   vout = -3 },
+                  { vin = 10,  vout = -4 }
+              }))
+          end,
+      }
+  }
+
+  --[[
+  pos: should be 0-1
+  points: an array of tables. the tables should have 2 indexes, 'pos' and 'val'.
+      - pos: the actual knob position to map to val
+      - val: the position that would be output if the knob were set to the actual position specified by pos
+      knobPos will be smoothly mapped to the the output between the points
+      the array of tables must be sorted by the table's pos and val, and sorting by either of the tables' indices should result in the same sort
+      the last table in the array should be {pos=1,val=1}
+      ex. one point with pos .5 and value .2 would cause the first 50% of the knob to map to 0-.2, and the second half to map to .2-1
+  --]]
+  function varisponse(vin, points)
+      vin = math.max(points[1].vin, math.min(vin, points[#points].vin))
+
+      for i = 2, #points do
+          if points[i].vin >= vin then -- hit
+              local vinBottom = points[i - 1].vin
+              local voutBottom = points[i - 1].vout
+              local vinRange = points[i].vin - vinBottom
+              local voutRange = points[i].vout - voutBottom
+              local segmentPercentage = (vin - vinBottom) / vinRange
+              local segmentVal = segmentPercentage * voutRange
+
+              return voutBottom + segmentVal
+          end
+      end
+  end
+
+  function startStream()
+      for i = 1, 2 do
+          -- crow.input[i] { mode = 'stream'
+          -- , time = 0.01
+          -- , stream = function(v) ins[i].callback(v) end }
+          crow.input[i].stream = function(v) ins[i].callback(v) end
+          crow.input[i].mode("stream", 0.01)
+      end
+  end
+
+  function stopStream()
+      for i = 1, 2 do
+          input[i] { mode = 'none' }
+      end
+  end
+
+  startStream()
 end
-
-function stopStream()
-    for i = 1, 2 do
-        input[i] { mode = 'none' }
-    end
-end
-
-startStream()
-
+-- crow_setup_for_joystick()
