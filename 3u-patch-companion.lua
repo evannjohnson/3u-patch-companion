65e995292f0edfb01770400ecd9d31ae6ca75561
Evan Johnson <ev.jonnson@gmail.com>
2025-05-04 21:37:01 -0600

    cleanup

--- 3u-patch-companion.lua
+++ 3u-patch-companion.lua
@@ -267,45 +267,38 @@ function trackball_input(typ, code, val)
 end
 hid.vports[1].event = trackball_input
 
---[[
-pos: should be 0-1
-points: an array of tables. the tables should have 2 indexes, 'pos' and 'val'.
-    - pos: the actual knob position to map to val
-    - val: the position that would be output if the knob were set to the actual position specified by pos
-    knobPos will be smoothly mapped to the the output between the points
-    the array of tables must be sorted by the table's pos and val, and sorting by either of the tables' indices should result in the same sort
-    the last table in the array should be {pos=1,val=1}
-    ex. one point with pos .5 and value .2 would cause the first 50% of the knob to map to 0-.2, and the second half to map to .2-1
---]]
-function varisponse(vin, points)
-    vin = math.max(points[1].vin, math.min(vin, points[#points].vin))
-
-    for i = 2, #points do
-        if points[i].vin >= vin then -- hit
-            local vinBottom = points[i - 1].vin
-            local voutBottom = points[i - 1].vout
-            local vinRange = points[i].vin - vinBottom
-            local voutRange = points[i].vout - voutBottom
-            local segmentPercentage = (vin - vinBottom) / vinRange
-            local segmentVal = segmentPercentage * voutRange
-
-            return voutBottom + segmentVal
-        end
-    end
-end
-
-local response_tables = {
-  trackball_x_to_wsyn_fm_index = {
-    { vin = 0,   vout = 0 },
-    { vin = 0.3, vout = 0 }, -- wiggle room for left side of joystick
-    { vin = 2.5, vout = 0.3 },
-    { vin = 6,   vout = 1 },
-    { vin = 10,  vout = 3 },
-  }
-}
-
+-- function clock_crow()
+--   clock.sync(1/4)
+-- end
 
 function crow_setup_for_joystick()
+    --[[
+  pos: should be 0-1
+  points: an array of tables. the tables should have 2 indexes, 'pos' and 'val'.
+      - pos: the actual knob position to map to val
+      - val: the position that would be output if the knob were set to the actual position specified by pos
+      knobPos will be smoothly mapped to the the output between the points
+      the array of tables must be sorted by the table's pos and val, and sorting by either of the tables' indices should result in the same sort
+      the last table in the array should be {pos=1,val=1}
+      ex. one point with pos .5 and value .2 would cause the first 50% of the knob to map to 0-.2, and the second half to map to .2-1
+  --]]
+  function varisponse(vin, points)
+      vin = math.max(points[1].vin, math.min(vin, points[#points].vin))
+
+      for i = 2, #points do
+          if points[i].vin >= vin then -- hit
+              local vinBottom = points[i - 1].vin
+              local voutBottom = points[i - 1].vout
+              local vinRange = points[i].vin - vinBottom
+              local voutRange = points[i].vout - voutBottom
+              local segmentPercentage = (vin - vinBottom) / vinRange
+              local segmentVal = segmentPercentage * voutRange
+
+              return voutBottom + segmentVal
+          end
+      end
+  end
+
   -- tune these response curves to taste
   -- vin is the value at the input, vout is the output value that voltage will be equal to
   -- values between the defined points are linearly interpolated
@@ -343,34 +336,7 @@ function crow_setup_for_joystick()
       }
   }
 
-  --[[
-  pos: should be 0-1
-  points: an array of tables. the tables should have 2 indexes, 'pos' and 'val'.
-      - pos: the actual knob position to map to val
-      - val: the position that would be output if the knob were set to the actual position specified by pos
-      knobPos will be smoothly mapped to the the output between the points
-      the array of tables must be sorted by the table's pos and val, and sorting by either of the tables' indices should result in the same sort
-      the last table in the array should be {pos=1,val=1}
-      ex. one point with pos .5 and value .2 would cause the first 50% of the knob to map to 0-.2, and the second half to map to .2-1
-  --]]
-  function varisponse(vin, points)
-      vin = math.max(points[1].vin, math.min(vin, points[#points].vin))
-
-      for i = 2, #points do
-          if points[i].vin >= vin then -- hit
-              local vinBottom = points[i - 1].vin
-              local voutBottom = points[i - 1].vout
-              local vinRange = points[i].vin - vinBottom
-              local voutRange = points[i].vout - voutBottom
-              local segmentPercentage = (vin - vinBottom) / vinRange
-              local segmentVal = segmentPercentage * voutRange
-
-              return voutBottom + segmentVal
-          end
-      end
-  end
-
-  function startStream()
+  function watch_joystick()
       for i = 1, 2 do
           -- crow.input[i] { mode = 'stream'
           -- , time = 0.01
@@ -380,12 +346,12 @@ function crow_setup_for_joystick()
       end
   end
 
-  function stopStream()
+  function no_watch_joystick()
       for i = 1, 2 do
           input[i] { mode = 'none' }
       end
   end
 
-  startStream()
+  watch_joystick()
 end
 -- crow_setup_for_joystick()
